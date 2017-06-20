09c0741816d8bd1990ee7076f539e7a8ff34bf4f
allthesignals <wilburnforce@gmail.com>
2017-06-20 11:22:56 -0400

    Change query to minify for smaller file size

--- app/adapters/town.js
+++ app/adapters/town.js
@@ -7,7 +7,7 @@ export default DS.CartoDBAdapter.extend({
   findAll: function(store, type) {
     // old query
     // queryTpl = 'SELECT heatwaveapp_towns.name, heatwaveapp_towns.cartodb_id, ST_Simplify(heatwaveapp_towns.the_geom,0.001), heatwaveapp_places.the_geom AS the_geom FROM {{table}}, heatwaveapp_places WHERE ST_Intersects(heatwaveapp_towns.the_geom, heatwaveapp_places.the_geom)',
-    var queryTpl = "SELECT heatwaveapp_towns.name, heatwaveapp_towns.municipal, heatwaveapp_towns.cartodb_id, ST_Simplify(heatwaveapp_towns.the_geom,0.001), heatwaveapp_places.the_geom AS the_geom FROM {{table}}, heatwaveapp_places WHERE heatwaveapp_towns.municipal IN ('" + config.MMC_towns.join("','") + "')",
+    var queryTpl = "SELECT heatwaveapp_towns.name, heatwaveapp_towns.municipal, heatwaveapp_towns.cartodb_id, ST_Simplify(heatwaveapp_towns.the_geom,0.1) AS the_geom FROM {{table}}, heatwaveapp_places WHERE heatwaveapp_towns.municipal IN ('" + config.MMC_towns.join("','") + "')",
         url = this.buildURL(type, queryTpl),
         response = {};
 
